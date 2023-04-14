493b30b035ffee87c5a37ce72d806ab84fc81c2e
s559348 <ashok.swarna1995@gmail.com>
2023-04-14 10:50:08 -0500

    update

--- controllers/shirt.js
+++ controllers/shirt.js
@@ -85,4 +85,40 @@ exports.shirt_create_post = async function(req, res) {
     }
    };
 
+   // for a specific Costume.
+exports.shirt_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Shirt.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+
+
+    // Handle Costume update form on PUT.
+exports.Shirt_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Shirt.findById( req.params.id)
+// Do updates of properties
+if(req.body.Shirt_type)
+toUpdate.Shirt_type = req.body.Shirt_type;
+if(req.Shirt.size) shirt_size = req.Shirt.size;
+if(req.Shirt.value) shirt_value = req.Shirt.value;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
+    
+
 
