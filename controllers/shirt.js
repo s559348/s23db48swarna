dc088cd92d10a74cf0b31527885aaac6bfb2be4e
s559348 <ashok.swarna1995@gmail.com>
2023-04-13 22:58:39 -0500

    update

--- controllers/shirt.js
+++ controllers/shirt.js
@@ -1,21 +1,88 @@
 var Shirt = require('../models/shirt');
 // List of all shirts
-exports.shirt_list = function(req, res) {
- res.send('NOT IMPLEMENTED: shirt list');
+// List of all shirt
+exports.shirt_list = async function (req, res) {
+    try {
+        theshirt = await Shirt.find();
+        res.send(theshirt);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
+// exports.shirt_list = function(req, res) {
+//  res.send('NOT IMPLEMENTED: shirt list');
+// };
 // for a specific shirt.
-exports.shirt_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: shirt detail: ' + req.params.id);
+exports.shirt_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: shirt detail: ' + req.params.id);
 };
 // Handle shirt create on POST.
-exports.shirt_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: shirt create POST');
-};
+// exports.shirt_create_post = function(req, res) {
+//  res.send('NOT IMPLEMENTED: shirt create POST');
+// };
 // Handle shirt delete form on DELETE.
-exports.shirt_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: shirt delete DELETE ' + req.params.id);
+
+// VIEWS
+// Handle a show all view
+exports.shirt_view_all_Page = async function (req, res) {
+    try {
+        theCostumes = await Shirt.find();
+        res.render('shirt', { title: 'Shirt Search Results', results: theCostumes });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+exports.shirt_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: shirt delete DELETE ' + req.params.id);
 };
 // Handle shirt update form on PUT.
-exports.shirt_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: shirt update PUT' + req.params.id);
+exports.shirt_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: shirt update PUT' + req.params.id);
 };
+// Handle Costume create on POST.
+exports.shirt_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Shirt();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.shirt_type = req.body.shirt_type;
+    document.shirt_size = req.body.shirt_size;
+    document.shirt_value = req.body.shirt_value;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle Costume create on POST.
+exports.shirt_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Shirt();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.shirt_type = req.body.shirt_type;
+    document.shirt_size = req.body.shirt_size;
+    document.shirt_value = req.body.shirt_value;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+
+
--- routes/shirt.js
+++ routes/shirt.js
@@ -3,9 +3,18 @@ var router = express.Router();
 
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Shirt', { title: 'Search Results Shirts' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('Shirt', { title: 'Search Results Shirts' });
+// });
+
+// module.exports = router;
+
+
+
+const shirt_controlers= require('../controllers/shirt');
+
+/* GET costumes */
+router.get('/', shirt_controlers.shirt_view_all_Page );
 
 module.exports = router;
